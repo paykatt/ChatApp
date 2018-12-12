cbb8017f9e1923d5f1ff0809185228997d316803
paykatt <45831858+paykatt@users.noreply.github.com>
2018-12-13 01:23:50 +0500

    Update RegistrationActivity.java

--- main/java/com/example/user/chatapp/RegistrationActivity.java
+++ main/java/com/example/user/chatapp/RegistrationActivity.java
@@ -18,10 +18,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * Created by tutlane on 08-01-2018.
- */
-
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText txtName, txtEmail, txtPwd;
